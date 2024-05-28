098ec9890ab79de622bb2abc18ad8a52f20e0524
Babatunde Ojo <babatundeojo30@gmail.com>
2024-05-28 20:40:00 +0100

    add POM to heroku the-internet

--- cypress/e2e/the-internet heroku/home.js
+++ cypress/e2e/the-internet heroku/home.js
@@ -1,40 +1,51 @@
 /// <reference types="cypress" />
 
+// import page
+import HomePage from "../../pages/the-internet heroku/home";
+
 const { Given, When, Then, Before } = require("@badeball/cypress-cucumber-preprocessor");
+
+// declare home pahe
+let homePage;
+
+Before(() => {
+    // create an instance of home page
+    homePage = new HomePage();
+});
 
 Given("I visit {string}", (url) => {
     cy.visit(url);
 });
 
 Then("I should be on {string} page", (text) => {
-    cy.get("h1.heading").should("have.text", text);
+    homePage.validatePage(text);
 });
 
 When("I click on {string} link", (linkName) => {
-    cy.contains("a", linkName).invoke("removeAttr", "target").click({ force: true });
+    homePage.clickOnLink(linkName);
 });
 
 Then("I should be redirected to {string} Page", (text) => {
-    cy.get("h1.hero__title").should("have.text", text);
+    homePage.validatePage(text);
 });
 
 When("I enter {string} as my email address", (email) => {
-    cy.get("input[type='email']").type(email);
+    homePage.setEmailAddress(email);
 });
 
 When("I select {string} as the language", (language) => {
-    cy.get("#options").select(language);
+    homePage.selectLanguage(language);
 });
 
 When("I click on {string} submit button", (buttonName) => {
-    cy.get(`input[type='submit'][value='${buttonName}']`).click();
+    homePage.clickOnButton();
 });
 
 Then("I should be redirected to {string} page with url {string}", (text, url) => {
-    cy.get("h1.pb-2").should("have.text", text);
-    cy.url().should("eq", url);
+    homePage.validatePage(text);
+    homePage.validateUrl(url);
 });
 
 Then("I should get {string} error message", (errorMessage) => {
-    cy.get("span.error").should("have.text", errorMessage)
+    homePage.validateErrorMessage(errorMessage);
 });
--- cypress/pages/the-internet heroku/home.js
+++ cypress/pages/the-internet heroku/home.js
@@ -1,3 +1,71 @@
 class Home {
-    
+    // locate H1 heading
+    getH1Heading() {
+        return cy.get("h1");
+    }
+
+    // locate anchor tag
+    getAnchorTag() {
+        return cy.get("a");
+    }
+
+    // locate email input tag
+    getEmailInputTag() {
+        return cy.get("input[type='email']");
+    }
+
+    // locate submit input tag
+    getSubmitInputTag() {
+        return cy.get("input[type='submit']")
+    }
+
+    // locate span tag
+    getSpanTag(className) {
+        return cy.get(`span.${className}`);
+    }
+
+    // locate select tag
+    getSelectTag() {
+        return cy.get("select");
+    }
+
+    // get url
+    getUrl() {
+        return cy.url();
+    }
+
+    validatePage(expectedText) {
+        this.getH1Heading().should("have.text", expectedText);
+    }
+
+    clickOnLink(linkName) {
+        this.getAnchorTag().each(($link) => {
+            const linkText = $link.text();
+            if (linkText === linkName) {
+                cy.wrap($link).invoke("removeAttr", "target").click();
+            }
+        });
+    }
+
+    setEmailAddress(email) {
+        this.getEmailInputTag().type(email);
+    }
+
+    selectLanguage(language) {
+        this.getSelectTag().select(language);
+    }
+
+    clickOnButton() {
+        this.getSubmitInputTag().click();
+    }
+
+    validateUrl(expectedUrl) {
+        this.getUrl().should("eq", expectedUrl);
+    }
+
+    validateErrorMessage(errorMessage) {
+        this.getSpanTag("error").should("have.text", errorMessage);
+    }    
 }
+
+export default Home;
